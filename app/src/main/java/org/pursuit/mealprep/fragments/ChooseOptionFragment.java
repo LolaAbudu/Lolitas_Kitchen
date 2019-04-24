ddc3050c42e9ad58bbc0e39d5bdc2002db3cb7c6
LolaAbudu <lolaabudu1589@gmail.com>
2019-04-24 13:03:14 -0400

    changed kitchen textSize

--- app/src/main/java/org/pursuit/mealprep/fragments/ChooseOptionFragment.java
+++ app/src/main/java/org/pursuit/mealprep/fragments/ChooseOptionFragment.java
@@ -13,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import org.pursuit.mealprep.FragmentInteractionListener;
 import org.pursuit.mealprep.R;
